36a976377b79102a4c7c79dc5dc62866584c044e
Jake Eik <eik995@hotmail.com>
2023-05-01 21:32:55 -0500

    developed JS

--- script.js
+++ script.js
@@ -2,30 +2,36 @@ let lang = 0
 let long = 0
 
 const app = {
+
     init: () => {
         document.getElementById('local').addEventListener('click', app.Search);
     },
 
+    // Get locations lat and lon from Geocoding API
     getLocation: () => {
-    let city = document.getElementById('local').value
-    let key = '3d81a03d1a16ff751a44888d76e80814';
-    let url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
-    fetch(url)
-    .then(Response =>{
-        if(!Response.ok) throw new Error(Response.statusText);
-        return Response.json();
-    })
-    .then(data => {
-        lang = data.lat;
-        long = data.lon;
-    })
-    .catch(console.err);
+        let city = document.getElementById('local').value
+        let key = '3d81a03d1a16ff751a44888d76e80814';
+        let url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
+        fetch(url)
+        .then(Response =>{
+            if(!Response.ok) throw new Error(Response.statusText);
+            return Response.json();
+        })
+        .then(data => {
+            lang = data.lat;
+            long = data.lon;
+        })
+        .catch(console.err);
     },
 
-    //Search:
+    //Search: (ev) => {
+//      getLocation()
+//      getWeather()
+//    }
 
 
-    getWeather: (ev) => {
+    // Gets weather object from OpenWeather API
+    getWeather: () => {
         let key = 'b275e1ea202493b69a7ea5954c475022'
         let lang = 'en'
         let units = 'imperial'
@@ -41,11 +47,11 @@ const app = {
         .catch(console.err)
     },
 
-
+    // Fills cards with weather info from getWeather()
     buildWeather: (resp) => {
         for (var i = 0; i < 5; i++) {
             if (i === 0 ) {
-                let toady = document.getElementById('weather1')
+                let toady = document.getElementById('weather0')
 
             }else {
                 let day = document.getElementById(`weather${[i]}`)
