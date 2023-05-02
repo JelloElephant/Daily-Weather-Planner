32bec9f36a37bfd9eb9cea3e5d88d2c64c4e15b7
Jake Eik <eik995@hotmail.com>
2023-05-01 21:32:13 -0500

    developed JS

---
+++ script.js
@@ -0,0 +1,59 @@
+let lang = 0
+let long = 0
+
+const app = {
+    init: () => {
+        document.getElementById('local').addEventListener('click', app.Search);
+    },
+
+    getLocation: () => {
+    let city = document.getElementById('local').value
+    let key = '3d81a03d1a16ff751a44888d76e80814';
+    let url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
+    fetch(url)
+    .then(Response =>{
+        if(!Response.ok) throw new Error(Response.statusText);
+        return Response.json();
+    })
+    .then(data => {
+        lang = data.lat;
+        long = data.lon;
+    })
+    .catch(console.err);
+    },
+
+    //Search:
+
+
+    getWeather: (ev) => {
+        let key = 'b275e1ea202493b69a7ea5954c475022'
+        let lang = 'en'
+        let units = 'imperial'
+        let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lang}&lon=${long}&appid=${key}&units=${units}&lang=${lang}`
+        fetch(url)
+        .then(resp => {
+            if (!resp.ok) throw new Error(resp.statusText);
+            return resp.json();
+        })
+        .then(data => {
+            app.buildWeather(data);
+        })
+        .catch(console.err)
+    },
+
+
+    buildWeather: (resp) => {
+        for (var i = 0; i < 5; i++) {
+            if (i === 0 ) {
+                let toady = document.getElementById('weather1')
+
+            }else {
+                let day = document.getElementById(`weather${[i]}`)
+
+            }
+        }
+
+    }
+}
+
+app.init();
