144d7ecdabc65187d573782f96bcfd90b78e4e27
Jake Eik <eik995@hotmail.com>
2023-07-25 15:08:39 -0500

    Implementing localStorage

--- script.js
+++ script.js
@@ -1,65 +1,91 @@
-let lang = 0
-let long = 0
 
-const app = {
-
-    init: () => {
-        document.getElementById('local').addEventListener('click', app.Search);
-    },
-
-    // Get locations lat and lon from Geocoding API
-    getLocation: () => {
-        let city = document.getElementById('local').value
-        let key = '3d81a03d1a16ff751a44888d76e80814';
-        let url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
-        fetch(url)
-        .then(Response =>{
-            if(!Response.ok) throw new Error(Response.statusText);
-            return Response.json();
-        })
-        .then(data => {
-            lang = data.lat;
-            long = data.lon;
-        })
-        .catch(console.err);
-    },
-
-    //Search: (ev) => {
-//      getLocation()
-//      getWeather()
-//    }
+const search = document.querySelector(".searchBar button")
+const sHistory = document.querySelector(".searchBar .searched")
+const searchHistory = []
 
 
-    // Gets weather object from OpenWeather API
-    getWeather: () => {
-        let key = 'b275e1ea202493b69a7ea5954c475022'
-        let lang = 'en'
-        let units = 'imperial'
-        let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lang}&lon=${long}&appid=${key}&units=${units}&lang=${lang}`
-        fetch(url)
-        .then(resp => {
-            if (!resp.ok) throw new Error(resp.statusText);
-            return resp.json();
-        })
-        .then(data => {
-            app.buildWeather(data);
-        })
-        .catch(console.err)
-    },
+search.addEventListener('click', () => {
+  const local = document.querySelector('.searchBar input').value
 
-    // Fills cards with weather info from getWeather()
-    buildWeather: (resp) => {
-        for (var i = 0; i < 5; i++) {
-            if (i === 0 ) {
-                let toady = document.getElementById('weather0')
+  if (!local) {
+    alert('Local not found')
+    return
+  }
 
-            }else {
-                let day = document.getElementById(`weather${[i]}`)
+  searchHistory.push(local)
+  localStorage.setItem('searched', JSON.stringify(searchHistory))
 
-            }
-        }
+  fiveDayFetch(local)
+  previousSearchs()
 
+})
+
+
+
+function fiveDayFetch(local) {
+  const key = '3d81a03d1a16ff751a44888d76e80814';
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${local}&units=imperial&cnt=6&appid=${key}`)
+  .then(ans => ans.json()).then(json => {
+    if(json.cod === '404') {
+      alert('Error!  Response not found!')
+      return
     }
+    console.log(json)
+
+    const localDate = document.querySelector('.weatherMain .localDate')
+    const blurb = document.querySelector('.weatherMain img')
+    const currTemp = document.querySelector('.weatherMain .currTemp')
+    const hot = document.querySelector('.weatherMain .maxTemp')
+    const cold = document.querySelector('.weatherMain .minTemp')
+    const humid = document.querySelector('.weatherMain .humidity')
+    const wind = document.querySelector('.weatherMain .wind')
+
+    localDate.innerHTML = `${new Date(json.list[0].dt * 1000)}`
+    blurb.src = `https://openweathermap.org/img/wn/${json.list[0].weather[0].icon}@2x.png`
+    currTemp.innerHTML = `<span>Currently ${json.list[0].main.temp}°F</span>`
+    hot.innerHTML = `<span>High ${json.list[0].main.temp_max}°F</span>`
+    cold.innerHTML = `<span>Low ${json.list[0].main.temp_min}°F</span>`
+    humid.innerHTML = `${json.list[0].main.humidity}% Humidity`
+    wind.innerHTML = `${json.list[0].wind.speed} mph`
+
+
+    for (i=1; i < 6; i++) {
+      const date = document.querySelector(`.weather${i} .localDate`)
+      const blurb = document.querySelector(`.weather${i} img`)
+      const hot = document.querySelector(`.weather${i} .maxTemp`)
+      const cold = document.querySelector(`.weather${i} .minTemp`)
+      const humid = document.querySelector(`.weather${i} .humidity`)
+      const wind = document.querySelector(`.weather${i} .wind`)
+
+      date.innerHTML = `${new Date(json.list[i].dt * 1000)}`
+      blurb.src = `https://openweathermap.org/img/wn/${json.list[i].weather[0].icon}@2x.png`
+      currTemp.innerHTML = `${parseInt(json.list[i].main.temp)}<span>°F</span>`
+      hot.innerHTML = `<span>High ${parseInt(json.list[i].main.temp_max)}°F</span>`
+      cold.innerHTML = `<span>Low ${parseInt(json.list[i].main.temp_min)}°F</span>`
+      humid.innerHTML = `${parseInt(json.list[i].main.humidity)}% Humidity`
+      wind.innerHTML = `${parseInt(json.list[i].wind.speed)} mph`
+    }
+  })
 }
 
-app.init();
+
+
+function previousSearchs() {
+  sHistory.innerHTML = ''
+  for (let i=0; i < searchHistory.length; i++) {
+    let button = document.createElement("button")
+    button.textContent = searchHistory[i]
+    button.classList.add('searchedHistory')
+    sHistory.append(button)
+  }
+
+  const sHistBtn = document.querySelector(".searchBar .searched .searchedHistory")
+  sHistBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    let local = event.target.textContent
+    fiveDayFetch(local)
+  })
+}
+
+
+//app.init();
